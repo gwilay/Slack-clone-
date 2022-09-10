2df1ab4351c3a71063620009ccbea34f8742475e
gwilay <angelavalencia876@gmail.com>
2022-09-10 19:27:32 +0800

    firebasedb

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import styled from "styled-components";
+import { db } from '../firebase';
 
 
-function SidebarOption ({Icon, title, AddChannelOption}) {
+function SidebarOption ({Icon, title, addChannelOption}) {
 
-    const addChannel = () => {};
+    const addChannel = () => {
+      const channelName = prompt('Please enter the channel name');
+
+      if (channelName){ 
+        db.collection('rooms').add({
+          name: channelName,
+        })
+      }
+    };
 
     const selectChannel = () => {};
 
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+        onClick={addChannelOption ? addChannel : selectChannel}
+    >
+        
+
         {Icon && <Icon fontSize ="small" style={{padding: 10}} />}
         {Icon ? (
           <h3>{title}</h3>
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,20 @@
+import { initializeApp } from "firebase/app"; 
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, collection, getDocs } from "firebase/firestore"; //For firestore
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA1EUKm_qfYOHerSE_BHEQ21ylNq7zhexE",
-    authDomain: "slack-clone-2ff39.firebaseapp.com",
-    projectId: "slack-clone-2ff39",
-    storageBucket: "slack-clone-2ff39.appspot.com",
-    messagingSenderId: "1082460755826",
-    appId: "1:1082460755826:web:2ff95b1dae98e92cbfefeb",
-    measurementId: "G-6JNT0GVFG6"
-  };
+  apiKey: "AIzaSyA1EUKm_qfYOHerSE_BHEQ21ylNq7zhexE",
+  authDomain: "slack-clone-2ff39.firebaseapp.com",
+  projectId: "slack-clone-2ff39",
+  storageBucket: "slack-clone-2ff39.appspot.com",
+  messagingSenderId: "1082460755826",
+  appId: "1:1082460755826:web:2ff95b1dae98e92cbfefeb",
+  measurementId: "G-6JNT0GVFG6",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
